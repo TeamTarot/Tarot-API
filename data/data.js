48eb74e5816d6b46331a60db29120a95e145646d
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-06 11:16:26 -0700

    added functionality to have a new user added to the db if not there in the beginning

--- data/data.js
+++ data/data.js
@@ -13,11 +13,18 @@ const superagent = require('superagent');
 // app.delete('/reading/:id', Data.deleteAReading);
 // app.put('/reading/:id', Data.updateAReading);
 
-Data.getUser = async(req, res) => {
- const email = req.query.email;
- await User.find({ email }, function (err, user) {
-    if (err) return console.error(err);
+Data.getUser = async (req, res) => {
+  const email = req.query.email;
+  await User.find({ email }, function (err, user) {
+    if (err) { return console.error(err) }
     // if the user doesn't have an email
+    if (!user) {
+      const welcome = new User({
+        email: req.query.email,
+        cards: []
+      });
+      welcome.save(() => console.log('welcome', {welcome}));
+    }
     // make them a profile with everything blank except the email
     // save the user to the database
     console.log('user infos: ', user);
@@ -25,20 +32,20 @@ Data.getUser = async(req, res) => {
   });
 }
 
-Data.handleAPICall = async(req, res) => {
+Data.handleAPICall = async (req, res) => {
   const url = `https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3`;
   superagent
-  .get(url)
-  .then(function (superagentResults) {
-    // handle 3 random cards
-    const cardResults = superagentResults.body.cards;
-    console.log('card results: ', cardResults);
-    res.status(200).send(cardResults);
-  })
-  .catch(function (error) {
-    console.log('something went wrong with superagent call in movies');
-    res.status(500).send('we messed up');
-  })
+    .get(url)
+    .then(function (superagentResults) {
+      // handle 3 random cards
+      const cardResults = superagentResults.body.cards;
+      console.log('card results: ', cardResults);
+      res.status(200).send(cardResults);
+    })
+    .catch(function (error) {
+      console.log('something went wrong with superagent call in movies');
+      res.status(500).send('we messed up');
+    })
 }
 
 // press draw 3 button
@@ -53,13 +60,14 @@ Data.handleAPICall = async(req, res) => {
 // server adds 3 card array to reading Schema
 // send the array to the db .put 
 
-Data.createAReading = async(req, res) => {
+Data.createAReading = async (req, res) => {
   const email = req.body.email;
   const reading = {
     // make sure this matches in the front end
-    cardSet: request.body.card.cardSet, 
-    date: request.body.card.date, 
-    journal: request.body.card.journal };
+    cardSet: request.body.card.cardSet,
+    date: request.body.card.date,
+    journal: request.body.card.journal
+  };
   User.findOne({ email }, (err, entry) => {
     if (err) return console.error(err);
     entry.cards.push(reading);
@@ -70,16 +78,16 @@ Data.createAReading = async(req, res) => {
   });
 }
 
-Data.deleteAReading = async(req, res) => {
+Data.deleteAReading = async (req, res) => {
   const id = req.params.id;
-  await User.deleteOne({_id:id});
+  await User.deleteOne({ _id: id });
   res.status(200).send('successfully deleted!');
 }
 
-Data.updateAReading = async(req, res) => {
+Data.updateAReading = async (req, res) => {
   const id = req.params.id;
   const data = req.body.journalUpdate;
-  User.findByIdAndUpdate({_id:id}, data, {new:true, useFindAndModify: false});
+  User.findByIdAndUpdate({ _id: id }, data, { new: true, useFindAndModify: false });
 }
 
 module.exports = Data;
