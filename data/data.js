98d56ace12b3fa376f50161aaeb3cc59aba5dd57
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-07 14:19:14 -0700

    updated the update route on the backend, its working

--- data/data.js
+++ data/data.js
@@ -6,6 +6,7 @@ const Data = {};
 require('dotenv').config();
 
 const superagent = require('superagent');
+const { response } = require('express');
 
 // app.get('/user', Data.getUser);
 // app.get('/draw', Data.handleAPICall);
@@ -95,9 +96,16 @@ Data.deleteAReading = async (req, res) => {
 
 
 Data.updateAReading = async (req, res) => {
-  const id = req.params.id;
-  const data = req.body.journalUpdate;
-  User.findByIdAndUpdate({ _id: id }, data, { new: true, useFindAndModify: false });
+  const index = req.params.index;
+  const email = req.body.email;
+
+  const entry = req.body.entry;
+
+  await User.findOne({email}, (err, data) => {
+    data.cards.splice(+index, 1, entry);
+    data.save();
+    res.status(200).send(data.cards);
+  })
 }
 
 module.exports = Data;
